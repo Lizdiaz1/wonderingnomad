cb818964e98bcb4fc813054f4a9d8ef5af20ed49
Lizette Diaz <mdlizette1@gmail.com>
2024-06-17 13:30:16 -0400

    Implement logout, navigation, and profile dropdown

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,50 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import LandingPage from './components/LandingPage';
 import SpotDetail from './components/SpotDetail';
 import NewSpot from './components/NewSpot';
 import ManageSpots from './components/ManageSpots';
 import UpdateSpot from './components/UpdateSpot';
+import LoginFormPage from './components/LoginFormPage';
+import SignupFormPage from './components/SignupFormPage';
+import Navigation from './components/Navigation';
+import * as sessionActions from './store/session';
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: '/', element: <LandingPage /> },
+      { path: '/login', element: <LoginFormPage /> },
+      { path: '/signup', element: <SignupFormPage /> },
+      { path: '/spots/:id', element: <SpotDetail /> },
+      { path: '/spots/new', element: <NewSpot /> },
+      { path: '/manage-spots', element: <ManageSpots /> },
+      { path: '/spots/:id/edit', element: <UpdateSpot /> }
+    ]
+  }
+]);
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        {/* Define your routes here */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/spots/:id" element={<SpotDetail />} />
-        <Route path="/spots/new" element={<NewSpot />} />
-        <Route path="/manage-spots" element={<ManageSpots />} />
-        <Route path="/spots/:id/edit" element={<UpdateSpot />} />
-        {/* Add more routes as needed */}
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
