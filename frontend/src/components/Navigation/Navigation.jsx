e865a90f4a1cfae63995e6688557601a78fcd934
Lizette Diaz <mdlizette1@gmail.com>
2024-06-17 13:40:45 -0400

    Implement login and signup modals

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,37 +1,39 @@
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import * as sessionActions from '../../store/session';
+import OpenModalButton from '../OpenModalButton';
+import LoginModal from '../LoginModal';
+import SignupModal from '../SignupModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
-  const sessionUser = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
-
-  const sessionLinks = sessionUser ? (
-    <>
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
       <li>
         <ProfileButton user={sessionUser} />
       </li>
-      <li>
-        <button onClick={logout}>Log Out</button>
-      </li>
-    </>
-  ) : (
-    <>
-      <li>
-        <NavLink to="/login">Log In</NavLink>
-      </li>
-      <li>
-        <NavLink to="/signup">Sign Up</NavLink>
-      </li>
-    </>
-  );
+    );
+  } else {
+    sessionLinks = (
+      <>
+        <li>
+          <OpenModalButton
+            buttonText="Log In"
+            modalComponent={<LoginModal />}
+          />
+        </li>
+        <li>
+          <OpenModalButton
+            buttonText="Sign Up"
+            modalComponent={<SignupModal />}
+          />
+        </li>
+      </>
+    );
+  }
 
   return (
     <ul>
@@ -44,3 +46,51 @@ function Navigation({ isLoaded }) {
 }
 
 export default Navigation;
+
+
+// import { NavLink } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+// import ProfileButton from './ProfileButton';
+// import * as sessionActions from '../../store/session';
+// import './Navigation.css';
+
+// function Navigation({ isLoaded }) {
+//   const sessionUser = useSelector(state => state.session.user);
+//   const dispatch = useDispatch();
+
+//   const logout = (e) => {
+//     e.preventDefault();
+//     dispatch(sessionActions.logout());
+//   };
+
+//   const sessionLinks = sessionUser ? (
+//     <>
+//       <li>
+//         <ProfileButton user={sessionUser} />
+//       </li>
+//       <li>
+//         <button onClick={logout}>Log Out</button>
+//       </li>
+//     </>
+//   ) : (
+//     <>
+//       <li>
+//         <NavLink to="/login">Log In</NavLink>
+//       </li>
+//       <li>
+//         <NavLink to="/signup">Sign Up</NavLink>
+//       </li>
+//     </>
+//   );
+
+//   return (
+//     <ul>
+//       <li>
+//         <NavLink to="/">Home</NavLink>
+//       </li>
+//       {isLoaded && sessionLinks}
+//     </ul>
+//   );
+// }
+
+// export default Navigation;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider as ReduxProvider } from 'react-redux';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
-import configureStore from './store/store';
+import './index.css';
+import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
-import { csrfFetch, restoreCSRF } from './store/csrf';
 import { Modal, ModalProvider } from './context/Modal';
 
 const store = configureStore();
 
-if (import.meta.env.MODE !== "production") {
+if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
@@ -19,15 +19,47 @@ if (import.meta.env.MODE !== "production") {
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-	<ModalProvider>
-		<ReduxProvider store={store}>
-				<App />
-				<Modal />
-		</ReduxProvider>
-	</ModalProvider>
-</React.StrictMode>
-  );
+  <React.StrictMode>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
+  </React.StrictMode>
+);
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { Provider as ReduxProvider } from 'react-redux';
+// import './index.css';
+// import App from './App';
+// import configureStore from './store/store';
+// import * as sessionActions from './store/session';
+// import { csrfFetch, restoreCSRF } from './store/csrf';
+// import { Modal, ModalProvider } from './context/Modal';
+
+// const store = configureStore();
+
+// if (import.meta.env.MODE !== "production") {
+//   restoreCSRF();
+
+//   window.csrfFetch = csrfFetch;
+//   window.store = store;
+//   window.sessionActions = sessionActions;
+// }
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+// <React.StrictMode>
+// 	<ModalProvider>
+// 		<ReduxProvider store={store}>
+// 				<App />
+// 				<Modal />
+// 		</ReduxProvider>
+// 	</ModalProvider>
+// </React.StrictMode>
+//   );
 
 // function Root() {
 // 	return (
